c4183358e275916d6d7bf9a8dd87cf8024da98d2
PanidaC <71167854+PanidaC@users.noreply.github.com>
2021-03-19 17:15:36 +0700

    add select number for print time

--- receive-data.php
+++ receive-data.php
@@ -25,6 +25,10 @@
             echo "email = ".$_POST["email"];
             echo "<br>";
             echo "password = ".$_POST["pwd"];
+
+            for ($x = 1; $x <= $_POST["time"]; $x++) {
+                echo "The number is: $x <br>";
+              }
         }
     ?>
     </div>
